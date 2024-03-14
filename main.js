84b88bd1355f9923539ccfaee271432cbc973da9
Humayun Khan <humayunk.pvt@gmail.com>
2024-03-15 00:59:54 +0530

    Added Icons Marquee

--- main.js
+++ main.js
@@ -2,9 +2,19 @@ const burgerLinks = document.querySelector('.burger-links');
 const burger = document.querySelector('.burger');
 const body = document.querySelector('body');
 
-burgerButton = function()
-{
-  burgerLinks.classList.toggle('active');
-  burger.classList.toggle('active');
-  body.classList.toggle('active');
+burgerButton = function () {
+    burgerLinks.classList.toggle('active');
+    burger.classList.toggle('active');
+    body.classList.toggle('active');
+}
+
+// Marquee Code
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+    marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
